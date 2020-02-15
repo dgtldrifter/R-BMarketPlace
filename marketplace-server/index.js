0ea8f0938d3c224e3504b4b5d04cc1e6b07fc9b6
Walburn, Micheal <26194597+dgtldrifter@users.noreply.github.com>
2020-02-15 12:11:59 -0600

    Reconfiguring project with proxy settings, when deployed to heroku, should now build both parts

---
+++ marketplace-server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+//serve the static files from the react application
+app.use(express.static(path.join(__dirname, '../marketplace-client/build')));
+
+//Catchall handler
+app.get('*', (req, res) => {
+   res.sendFile(path.join(__dirname, '../marketplace-client/build/index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log(`Listening on port ${port}`);
