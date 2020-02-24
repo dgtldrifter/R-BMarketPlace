3037c30cccea97fe832401a6e1c2af1a6e3a0a87
Walburn, Micheal <26194597+dgtldrifter@users.noreply.github.com>
2020-02-24 15:09:06 -0600

    added separate file to manage db queries

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const path = require('path');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
-require('dotenv').config();
 
 const app = express();
+
+require('dotenv').config();
 
 //serve the static files from the react application
 app.use(express.static(path.join(__dirname, 'marketplace-client/build')));
@@ -15,13 +16,19 @@ app.get('*', (req, res) => {
    res.sendFile(path.join(__dirname, 'marketplace-client/build/index.html'));
 });
 
+app.post('/user/signup', (req, res) => {
+let user = req.body.user;
+let password = req.body.password;
 
-const port = process.env.PORT || 5000;
+
+});
+
+
 
 app.use(cors());
 app.use(express.json());
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }); 
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
 const connection = mongoose.connection;
 connection.once('open', () => {
    console.log("Database conneciton estabished successfully!");
@@ -29,6 +36,7 @@ connection.once('open', () => {
 
 const userRouter = require('./routes/users');
 app.use("/users", userRouter);
-app.listen(port);
 
+const port = process.env.PORT || 5000;
+app.listen(port);
 console.log(`Listening on port ${port}`);
