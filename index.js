3d402b1ca6700c0c2721fca23577140b9a0e66ea
bdahal0402 <bdahal0402@gmail.com>
2020-02-24 12:39:20 -0600

    Added a user model and backend registration

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
 
 const app = express();
 
@@ -11,7 +15,20 @@ app.get('*', (req, res) => {
    res.sendFile(path.join(__dirname, 'marketplace-client/build/index.html'));
 });
 
+
 const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }); 
+const connection = mongoose.connection;
+connection.once('open', () => {
+   console.log("Database conneciton estabished successfully!");
+})
+
+const userRouter = require('./routes/users');
+app.use("/users", userRouter);
 app.listen(port);
 
 console.log(`Listening on port ${port}`);
