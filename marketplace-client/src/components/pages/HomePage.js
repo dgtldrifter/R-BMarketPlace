0fd66b95cab601b16af763f60197f3a6d474333e
burtonzr <burtonzr@gmail.com>
2020-02-26 17:16:02 -0600

    Cleaned up code

--- marketplace-client/src/components/pages/HomePage.js
+++ marketplace-client/src/components/pages/HomePage.js
@@ -112,125 +112,111 @@ class HomePage extends Component {
                     </div>
                   </div>
                 </div>
-
-          <div className="col-lg-4 col-md-6 portfolio-item filter-card">
-            <div className="portfolio-wrap">
-              <img src="img/portfolio/portfolio-4.jpg" className="img-fluid" alt=""/>
-              <div className="portfolio-info">
-                <h4>Card 2</h4>
-                <p>Card</p>
+                <div className="col-lg-4 col-md-6 portfolio-item filter-card">
+                  <div className="portfolio-wrap">
+                    <img src="img/portfolio/portfolio-4.jpg" className="img-fluid" alt=""/>
+                    <div className="portfolio-info">
+                      <h4>Card 2</h4>
+                      <p>Card</p>
+                    </div>
+                    <div className="portfolio-links">
+                      <a href="img/portfolio/portfolio-4.jpg" data-gall="portfolioGallery" className="venobox" title="Card 2"><i class="bx bx-plus"></i></a>
+                      <a href="#" title="More Details"><i className="bx bx-link"></i></a>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-lg-4 col-md-6 portfolio-item filter-web">
+                  <div className="portfolio-wrap">
+                    <img src="img/portfolio/portfolio-5.jpg" className="img-fluid" alt=""/>
+                    <div className="portfolio-info">
+                      <h4>Web 2</h4>
+                      <p>Web</p>
+                    </div>
+                    <div className="portfolio-links">
+                      <a href="img/portfolio/portfolio-5.jpg" data-gall="portfolioGallery" className="venobox" title="Web 2"><i class="bx bx-plus"></i></a>
+                      <a href="#" title="More Details"><i className="bx bx-link"></i></a>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-lg-4 col-md-6 portfolio-item filter-app">
+                  <div className="portfolio-wrap">
+                    <img src="img/portfolio/portfolio-6.jpg" className="img-fluid" alt=""/>
+                    <div className="portfolio-info">
+                      <h4>App 3</h4>
+                      <p>App</p>
+                    </div>
+                    <div className="portfolio-links">
+                      <a href="img/portfolio/portfolio-6.jpg" data-gall="portfolioGallery" className="venobox" title="App 3"><i class="bx bx-plus"></i></a>
+                      <a href="#" title="More Details"><i className="bx bx-link"></i></a>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-lg-4 col-md-6 portfolio-item filter-card">
+                  <div className="portfolio-wrap">
+                    <img src="img/portfolio/portfolio-7.jpg" className="img-fluid" alt=""/>
+                    <div className="portfolio-info">
+                      <h4>Card 1</h4>
+                      <p>Card</p>
+                    </div>
+                    <div className="portfolio-links">
+                      <a href="img/portfolio/portfolio-7.jpg" data-gall="portfolioGallery" className="venobox" title="Card 1"><i class="bx bx-plus"></i></a>
+                      <a href="#" title="More Details"><i className="bx bx-link"></i></a>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-lg-4 col-md-6 portfolio-item filter-card">
+                  <div className="portfolio-wrap">
+                    <img src="img/portfolio/portfolio-8.jpg" className="img-fluid" alt=""/>
+                    <div className="portfolio-info">
+                      <h4>Card 3</h4>
+                      <p>Card</p>
+                    </div>
+                    <div className="portfolio-links">
+                      <a href="img/portfolio/portfolio-8.jpg" data-gall="portfolioGallery" className="venobox" title="Card 3"><i class="bx bx-plus"></i></a>
+                      <a href="#" title="More Details"><i className="bx bx-link"></i></a>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-4 col-md-6 portfolio-item filter-web">
+                  <div class="portfolio-wrap">
+                    <img src="img/portfolio/portfolio-9.jpg" class="img-fluid" alt=""/>
+                    <div class="portfolio-info">
+                      <h4>Web 3</h4>
+                      <p>Web</p>
+                    </div>
+                    <div class="portfolio-links">
+                      <a href="img/portfolio/portfolio-9.jpg" data-gall="portfolioGallery" class="venobox" title="Web 3"><i class="bx bx-plus"></i></a>
+                      <a href="#" title="More Details"><i class="bx bx-link"></i></a>
+                    </div>
+                  </div>
+                </div>
               </div>
-              <div className="portfolio-links">
-                <a href="img/portfolio/portfolio-4.jpg" data-gall="portfolioGallery" className="venobox" title="Card 2"><i class="bx bx-plus"></i></a>
-                <a href="#" title="More Details"><i className="bx bx-link"></i></a>
+            </div>
+          </section>
+        </main>
+        <footer id="footer">
+          <div class="footer-top">
+            <div class="container">
+              <div class="row">
+                <div class="col-lg-3 col-md-6 footer-info">
+                  <img src="img/logo.jpg" alt="" class="img-fluid"/>
+                </div>
+                <div class="col-lg-3 col-md-6 footer-info">
+                  <div class="social-links mt-3">
+                    <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+                    <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+                    <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+                    <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+                    <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-
-          <div className="col-lg-4 col-md-6 portfolio-item filter-web">
-            <div className="portfolio-wrap">
-              <img src="img/portfolio/portfolio-5.jpg" className="img-fluid" alt=""/>
-              <div className="portfolio-info">
-                <h4>Web 2</h4>
-                <p>Web</p>
-              </div>
-              <div className="portfolio-links">
-                <a href="img/portfolio/portfolio-5.jpg" data-gall="portfolioGallery" className="venobox" title="Web 2"><i class="bx bx-plus"></i></a>
-                <a href="#" title="More Details"><i className="bx bx-link"></i></a>
-              </div>
-            </div>
-          </div>
-
-          <div className="col-lg-4 col-md-6 portfolio-item filter-app">
-            <div className="portfolio-wrap">
-              <img src="img/portfolio/portfolio-6.jpg" className="img-fluid" alt=""/>
-              <div className="portfolio-info">
-                <h4>App 3</h4>
-                <p>App</p>
-              </div>
-              <div className="portfolio-links">
-                <a href="img/portfolio/portfolio-6.jpg" data-gall="portfolioGallery" className="venobox" title="App 3"><i class="bx bx-plus"></i></a>
-                <a href="#" title="More Details"><i className="bx bx-link"></i></a>
-              </div>
-            </div>
-          </div>
-
-          <div className="col-lg-4 col-md-6 portfolio-item filter-card">
-            <div className="portfolio-wrap">
-              <img src="img/portfolio/portfolio-7.jpg" className="img-fluid" alt=""/>
-              <div className="portfolio-info">
-                <h4>Card 1</h4>
-                <p>Card</p>
-              </div>
-              <div className="portfolio-links">
-                <a href="img/portfolio/portfolio-7.jpg" data-gall="portfolioGallery" className="venobox" title="Card 1"><i class="bx bx-plus"></i></a>
-                <a href="#" title="More Details"><i className="bx bx-link"></i></a>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 portfolio-item filter-card">
-            <div class="portfolio-wrap">
-              <img src="img/portfolio/portfolio-8.jpg" class="img-fluid" alt=""/>
-              <div class="portfolio-info">
-                <h4>Card 3</h4>
-                <p>Card</p>
-              </div>
-              <div class="portfolio-links">
-                <a href="img/portfolio/portfolio-8.jpg" data-gall="portfolioGallery" class="venobox" title="Card 3"><i class="bx bx-plus"></i></a>
-                <a href="#" title="More Details"><i class="bx bx-link"></i></a>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 portfolio-item filter-web">
-            <div class="portfolio-wrap">
-              <img src="img/portfolio/portfolio-9.jpg" class="img-fluid" alt=""/>
-              <div class="portfolio-info">
-                <h4>Web 3</h4>
-                <p>Web</p>
-              </div>
-              <div class="portfolio-links">
-                <a href="img/portfolio/portfolio-9.jpg" data-gall="portfolioGallery" class="venobox" title="Web 3"><i class="bx bx-plus"></i></a>
-                <a href="#" title="More Details"><i class="bx bx-link"></i></a>
-              </div>
-            </div>
-          </div>
-        </div>
+        </footer>
+        <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
       </div>
-    </section>
-  </main>
-
-
-
-  <footer id="footer">
-    <div class="footer-top">
-      <div class="container">
-        <div class="row">
-		 <div class="col-lg-3 col-md-6 footer-info">
-            <img src="img/logo.jpg" alt="" class="img-fluid"/>
-          </div>
-          <div class="col-lg-3 col-md-6 footer-info">
-            <div class="social-links mt-3">
-              <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-              <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-              <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-              <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-              <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </footer>
-
-  <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
-
-
-
-
-        </div>
-        );
-    }
+    );
+  }
 }
 export default HomePage;
