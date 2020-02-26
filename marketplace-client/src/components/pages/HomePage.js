2d7ab968aa7afec36a5a17f699fd483a457d4de8
burtonzr <burtonzr@gmail.com>
2020-02-26 16:57:20 -0600

    Fixed Responsive Navbar Problem

--- marketplace-client/src/components/pages/HomePage.js
+++ marketplace-client/src/components/pages/HomePage.js
@@ -8,182 +8,163 @@ const styling = {
 
 var loadjs = require('loadjs');
 
-
-
 const homeImage = "https://images.unsplash.com/photo-1502252430442-aac78f397426?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80";
 
 class HomePage extends Component {
-
-
     componentDidMount() {
         loadjs('main.js');
     }
 
-
-
     render() {
-        return (
-            <div>
-                <div id="banner">
-                <Parallax bgImage={homeImage} style={styling} strength={450} onScroll={this.handleScroll}>
-                    <div id="scrollHeader">
-                        <h1 className="title">R&amp;B Market Place</h1>
-                    </div>
-                </Parallax>
-                </div>
-                <div id="header">
-                <div class="container d-flex">
-                  <div class="logo mr-auto">
-                    <a href="#banner"><img src="img/logo.jpg" alt="" class="img-fluid"/></a>
-                  </div>
-            
-                  <nav class="nav-menu d-none d-lg-block">
+      return (
+        <div>
+          <div id="banner">
+            <Parallax bgImage={homeImage} style={styling} strength={450} onScroll={this.handleScroll}>
+              <div id="scrollHeader">
+                <h1 className="title">R&amp;B Market Place</h1>
+              </div>
+            </Parallax>
+          </div>
+          <div id="header">
+            <div className="container d-flex">
+              <div className="logo mr-auto">
+                <a href="#banner"><img src="img/logo.jpg" alt="logo" className="img-fluid"/></a>
+              </div>
+              <nav className="nav-menu d-none d-lg-block">
+                <ul>
+                  <li className="active"><a href="#banner">Home</a></li>
+                  <li><a href="#main">Products and Properties</a></li>
+                  <li><Link to="/signup">Sign Up / Login</Link></li>
+                  <li className="drop-down"><a href="">Drop Down</a>
                     <ul>
-                      <li class="active"><a href="#banner">Home</a></li>
-                      <li><a href="#main">Products and Properties</a></li>
-                      <li><Link to="/signup">Sign Up</Link></li>
-                      <li><a href="#">Link</a></li>
-                      <li><a href="#header">Link</a></li>
-                      <li class="drop-down"><a href="">Drop Down</a>
+                      <li><a href="#">Drop Down 1</a></li>
+                      <li className="drop-down"><a href="#">Drop Down 2</a>
                         <ul>
-                          <li><a href="#">Drop Down 1</a></li>
-                          <li class="drop-down"><a href="#">Drop Down 2</a>
-                            <ul>
-                              <li><a href="#">Deep Drop Down 1</a></li>
-                              <li><a href="#">Deep Drop Down 2</a></li>
-                              <li><a href="#">Deep Drop Down 3</a></li>
-                              <li><a href="#">Deep Drop Down 4</a></li>
-                              <li><a href="#">Deep Drop Down 5</a></li>
-                            </ul>
-                          </li>
-                          <li><a href="#">Drop Down 3</a></li>
-                          <li><a href="#">Drop Down 4</a></li>
-                          <li><a href="#">Drop Down 5</a></li>
+                          <li><a href="#">Deep Drop Down 1</a></li>
+                          <li><a href="#">Deep Drop Down 2</a></li>
+                          <li><a href="#">Deep Drop Down 3</a></li>
+                          <li><a href="#">Deep Drop Down 4</a></li>
+                          <li><a href="#">Deep Drop Down 5</a></li>
                         </ul>
                       </li>
-                      <li><a href="#">Link</a></li>
+                      <li><a href="#">Drop Down 3</a></li>
+                      <li><a href="#">Drop Down 4</a></li>
+                      <li><a href="#">Drop Down 5</a></li>
                     </ul>
-                  </nav>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+          <main id="main">
+            <section id="portfolio" className="portfolio section-bg">
+              <div className="container">
+                <div className="section-title">
+                  <h2>Hottest Properties and Products</h2>
                 </div>
-              </div>
+                <div className="row">
+                  <div className="col-lg-12 d-flex justify-content-center">
+                    <ul id="portfolio-flters">
+                      <li data-filter="*" className="filter-active">All</li>
+                      <li data-filter=".filter-app">App</li>
+                      <li data-filter=".filter-card">Card</li>
+                      <li data-filter=".filter-web">Web</li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="row portfolio-container">
+                  <div className="col-lg-4 col-md-6 portfolio-item filter-app">
+                    <div className="portfolio-wrap">
+                      <img src="img/portfolio/portfolio-1.jpg" className="img-fluid" alt=""/>
+                      <div className="portfolio-info">
+                      <h4>App 1</h4>
+                      <p>App</p>
+                    </div>
+                    <div className="portfolio-links">
+                      <a href="img/portfolio/portfolio-1.jpg" data-gall="portfolioGallery" className="venobox" title="App 1"><i class="bx bx-plus"></i></a>
+                      <a href="#" title="More Details"><i className="bx bx-link"></i></a>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-lg-4 col-md-6 portfolio-item filter-web">
+                  <div className="portfolio-wrap">
+                    <img src="img/portfolio/portfolio-2.jpg" className="img-fluid" alt=""/>
+                    <div className="portfolio-info">
+                      <h4>Web 3</h4>
+                      <p>Web</p>
+                    </div>
+                    <div className="portfolio-links">
+                      <a href="img/portfolio/portfolio-2.jpg" data-gall="portfolioGallery" class="venobox" title="Web 3"><i class="bx bx-plus"></i></a>
+                      <a href="#" title="More Details"><i className="bx bx-link"></i></a>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-lg-4 col-md-6 portfolio-item filter-app">
+                  <div className="portfolio-wrap">
+                    <img src="img/portfolio/portfolio-3.jpg" className="img-fluid" alt=""/>
+                    <div className="portfolio-info">
+                      <h4>App 2</h4>
+                      <p>App</p>
+                    </div>
+                    <div className="portfolio-links">
+                      <a href="img/portfolio/portfolio-3.jpg" data-gall="portfolioGallery" class="venobox" title="App 2"><i class="bx bx-plus"></i></a>
+                      <a href="#" title="More Details"><i className="bx bx-link"></i></a>
+                    </div>
+                  </div>
+                </div>
 
-
-
-<main id="main">
-    <section id="portfolio" class="portfolio section-bg">
-      <div class="container">
-
-        <div class="section-title">
-          <h2>Hottest Properties and Products</h2>
-        </div>
-
-        <div class="row">
-          <div class="col-lg-12 d-flex justify-content-center">
-            <ul id="portfolio-flters">
-              <li data-filter="*" class="filter-active">All</li>
-              <li data-filter=".filter-app">App</li>
-              <li data-filter=".filter-card">Card</li>
-              <li data-filter=".filter-web">Web</li>
-            </ul>
-          </div>
-        </div>
-
-        <div class="row portfolio-container">
-
-          <div class="col-lg-4 col-md-6 portfolio-item filter-app">
-            <div class="portfolio-wrap">
-              <img src="img/portfolio/portfolio-1.jpg" class="img-fluid" alt=""/>
-              <div class="portfolio-info">
-                <h4>App 1</h4>
-                <p>App</p>
-              </div>
-              <div class="portfolio-links">
-                <a href="img/portfolio/portfolio-1.jpg" data-gall="portfolioGallery" class="venobox" title="App 1"><i class="bx bx-plus"></i></a>
-                <a href="#" title="More Details"><i class="bx bx-link"></i></a>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 portfolio-item filter-web">
-            <div class="portfolio-wrap">
-              <img src="img/portfolio/portfolio-2.jpg" class="img-fluid" alt=""/>
-              <div class="portfolio-info">
-                <h4>Web 3</h4>
-                <p>Web</p>
-              </div>
-              <div class="portfolio-links">
-                <a href="img/portfolio/portfolio-2.jpg" data-gall="portfolioGallery" class="venobox" title="Web 3"><i class="bx bx-plus"></i></a>
-                <a href="#" title="More Details"><i class="bx bx-link"></i></a>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 portfolio-item filter-app">
-            <div class="portfolio-wrap">
-              <img src="img/portfolio/portfolio-3.jpg" class="img-fluid" alt=""/>
-              <div class="portfolio-info">
-                <h4>App 2</h4>
-                <p>App</p>
-              </div>
-              <div class="portfolio-links">
-                <a href="img/portfolio/portfolio-3.jpg" data-gall="portfolioGallery" class="venobox" title="App 2"><i class="bx bx-plus"></i></a>
-                <a href="#" title="More Details"><i class="bx bx-link"></i></a>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 portfolio-item filter-card">
-            <div class="portfolio-wrap">
-              <img src="img/portfolio/portfolio-4.jpg" class="img-fluid" alt=""/>
-              <div class="portfolio-info">
+          <div className="col-lg-4 col-md-6 portfolio-item filter-card">
+            <div className="portfolio-wrap">
+              <img src="img/portfolio/portfolio-4.jpg" className="img-fluid" alt=""/>
+              <div className="portfolio-info">
                 <h4>Card 2</h4>
                 <p>Card</p>
               </div>
-              <div class="portfolio-links">
-                <a href="img/portfolio/portfolio-4.jpg" data-gall="portfolioGallery" class="venobox" title="Card 2"><i class="bx bx-plus"></i></a>
-                <a href="#" title="More Details"><i class="bx bx-link"></i></a>
+              <div className="portfolio-links">
+                <a href="img/portfolio/portfolio-4.jpg" data-gall="portfolioGallery" className="venobox" title="Card 2"><i class="bx bx-plus"></i></a>
+                <a href="#" title="More Details"><i className="bx bx-link"></i></a>
               </div>
             </div>
           </div>
 
-          <div class="col-lg-4 col-md-6 portfolio-item filter-web">
-            <div class="portfolio-wrap">
-              <img src="img/portfolio/portfolio-5.jpg" class="img-fluid" alt=""/>
-              <div class="portfolio-info">
+          <div className="col-lg-4 col-md-6 portfolio-item filter-web">
+            <div className="portfolio-wrap">
+              <img src="img/portfolio/portfolio-5.jpg" className="img-fluid" alt=""/>
+              <div className="portfolio-info">
                 <h4>Web 2</h4>
                 <p>Web</p>
               </div>
-              <div class="portfolio-links">
-                <a href="img/portfolio/portfolio-5.jpg" data-gall="portfolioGallery" class="venobox" title="Web 2"><i class="bx bx-plus"></i></a>
-                <a href="#" title="More Details"><i class="bx bx-link"></i></a>
+              <div className="portfolio-links">
+                <a href="img/portfolio/portfolio-5.jpg" data-gall="portfolioGallery" className="venobox" title="Web 2"><i class="bx bx-plus"></i></a>
+                <a href="#" title="More Details"><i className="bx bx-link"></i></a>
               </div>
             </div>
           </div>
 
-          <div class="col-lg-4 col-md-6 portfolio-item filter-app">
-            <div class="portfolio-wrap">
-              <img src="img/portfolio/portfolio-6.jpg" class="img-fluid" alt=""/>
-              <div class="portfolio-info">
+          <div className="col-lg-4 col-md-6 portfolio-item filter-app">
+            <div className="portfolio-wrap">
+              <img src="img/portfolio/portfolio-6.jpg" className="img-fluid" alt=""/>
+              <div className="portfolio-info">
                 <h4>App 3</h4>
                 <p>App</p>
               </div>
-              <div class="portfolio-links">
-                <a href="img/portfolio/portfolio-6.jpg" data-gall="portfolioGallery" class="venobox" title="App 3"><i class="bx bx-plus"></i></a>
-                <a href="#" title="More Details"><i class="bx bx-link"></i></a>
+              <div className="portfolio-links">
+                <a href="img/portfolio/portfolio-6.jpg" data-gall="portfolioGallery" className="venobox" title="App 3"><i class="bx bx-plus"></i></a>
+                <a href="#" title="More Details"><i className="bx bx-link"></i></a>
               </div>
             </div>
           </div>
 
-          <div class="col-lg-4 col-md-6 portfolio-item filter-card">
-            <div class="portfolio-wrap">
-              <img src="img/portfolio/portfolio-7.jpg" class="img-fluid" alt=""/>
-              <div class="portfolio-info">
+          <div className="col-lg-4 col-md-6 portfolio-item filter-card">
+            <div className="portfolio-wrap">
+              <img src="img/portfolio/portfolio-7.jpg" className="img-fluid" alt=""/>
+              <div className="portfolio-info">
                 <h4>Card 1</h4>
                 <p>Card</p>
               </div>
-              <div class="portfolio-links">
-                <a href="img/portfolio/portfolio-7.jpg" data-gall="portfolioGallery" class="venobox" title="Card 1"><i class="bx bx-plus"></i></a>
-                <a href="#" title="More Details"><i class="bx bx-link"></i></a>
+              <div className="portfolio-links">
+                <a href="img/portfolio/portfolio-7.jpg" data-gall="portfolioGallery" className="venobox" title="Card 1"><i class="bx bx-plus"></i></a>
+                <a href="#" title="More Details"><i className="bx bx-link"></i></a>
               </div>
             </div>
           </div>
