ce05a6098af68671fa618b3874578f3811e3f229
burtonzr <burtonzr@gmail.com>
2020-03-15 18:19:09 -0500

    Working on localStorage

--- marketplace-client/src/components/layouts/Navbar.js
+++ marketplace-client/src/components/layouts/Navbar.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Axios from 'axios';
 
 export default class navbar extends React.Component {
   render() {
--- marketplace-client/src/components/pages/HomePage.js
+++ marketplace-client/src/components/pages/HomePage.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import axios from 'axios';
 
 var loadjs = require('loadjs');
 
