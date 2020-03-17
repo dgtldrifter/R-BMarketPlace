427a946357b3203f43c5e6abc8a0c512035ed7f2
burtonzr <burtonzr@gmail.com>
2020-03-17 17:20:15 -0500

    Axios request works on HomePage.js

--- marketplace-client/src/components/pages/HomePage.js
+++ marketplace-client/src/components/pages/HomePage.js
@@ -3,6 +3,9 @@ import Axios from 'axios';
 
 var loadjs = require('loadjs');
 
+var name1 = "";
+var name2 = "";
+
 class HomePage extends Component {
   constructor(props) {
     super(props);
@@ -17,14 +20,16 @@ class HomePage extends Component {
     loadjs('main.js');
     Axios({
       method: 'POST',
-      url: 'posts/getAll'
+      url: 'posts/getAll',
+      headers: {
+        "Content-Type": "application/json"
+      }
     }).then((response) => {
       this.setState({
         isLoaded: true,
         posts: response
-      });
-      console.log(this.state.posts.data[0].name);
-      console.log(this.state.posts.data[1].name);
+      }); 
+      name1 = this.state.posts.data[0].name 
     }).catch((error) => {
       this.setState({
         isLoaded: true,
@@ -34,7 +39,7 @@ class HomePage extends Component {
   }
 
   render() {
-    const { error, isLoaded, posts } = this.state;
+    const { error, isLoaded } = this.state;
     if(error) {
       return <div>Error: {error.message}</div>;
     } else if(!isLoaded) {
@@ -42,50 +47,41 @@ class HomePage extends Component {
     } else {
       return (
         <div>
-          <ul>
-            {/*}
-            {posts.map(post => (
-              <li key={post.name}>
-                {post.name} {post.price}
-              </li>
-            ))}*/}
-          </ul>
-          <div>
-            <main id="main">
-              <section id="portfolio" className="portfolio section-bg">
-                <div className="container">
-                  <div className="section-title">
-                    <h2>Hottest Properties and Products</h2>
+          <main id="main">
+            <section id="portfolio" className="portfolio section-bg">
+              <div className="container">
+                <div className="section-title">
+                  <h2>Hottest Products</h2>
+                </div>
+                <div className="row">
+                  <div className="col-lg-12 d-flex justify-content-center">
+                    <ul id="portfolio-flters">
+                      <li data-filter="*" className="filter-active">All</li>
+                      <li data-filter=".filter-app">Coffee Appliances</li>
+                      <li data-filter=".filter-card">Cooking Appliances</li>
+                      <li data-filter=".filter-web">Silverware</li>
+                    </ul>
                   </div>
-                  <div className="row">
-                    <div className="col-lg-12 d-flex justify-content-center">
-                      <ul id="portfolio-flters">
-                        <li data-filter="*" className="filter-active">All</li>
-                        <li data-filter=".filter-app">App</li>
-                        <li data-filter=".filter-card">Card</li>
-                        <li data-filter=".filter-web">Web</li>
-                      </ul>
-                    </div>
-                  </div>
-                  <div className="row portfolio-container">
-                    <div className="col-lg-4 col-md-6 portfolio-item filter-app">
-                      <div className="portfolio-wrap">
-                        <img src="img/portfolio/portfolio-1.jpg" className="img-fluid" alt=""/>
-                        <div className="portfolio-info">
-                          <h4>App 1</h4>
-                          <p>App</p>
-                        </div>
-                      <div className="portfolio-links">
-                        <a href="img/portfolio/portfolio-1.jpg" data-gall="portfolioGallery" className="venobox" title="App 1"><i className="bx bx-plus"></i></a>
-                        {/* <a title="More Details"><i className="bx bx-link"></i></a> */}
+                </div>
+                <div className="row portfolio-container">
+                  <div className="col-lg-4 col-md-6 portfolio-item filter-app">
+                    <div className="portfolio-wrap">
+                      <img src="img/portfolio/portfolio-1.jpg" className="img-fluid" alt={name1}/>
+                      <div className="portfolio-info">
+                        <h4>{name1}</h4>
+                        <p>App</p>
                       </div>
+                    <div className="portfolio-links">
+                      <a href="img/portfolio/portfolio-1.jpg" data-gall="portfolioGallery" className="venobox" title="App 1"><i className="bx bx-plus"></i></a>
+                      {/* <a title="More Details"><i className="bx bx-link"></i></a> */}
                     </div>
                   </div>
+                </div>
                   <div className="col-lg-4 col-md-6 portfolio-item filter-web">
                     <div className="portfolio-wrap">
                       <img src="img/portfolio/portfolio-2.jpg" className="img-fluid" alt=""/>
                       <div className="portfolio-info">
-                        <h4>Web 3</h4>
+                        <h4>{name2}</h4>
                         <p>Web</p>
                       </div>
                       <div className="portfolio-links">
@@ -164,7 +160,6 @@ class HomePage extends Component {
             </section>
           </main>
           <a href="#" className="back-to-top"><i className="icofont-simple-up"></i></a>
-        </div>
         </div>
       );
     }
