26853be6952bfc29d15c9a548711a36353507954
burtonzr <burtonzr@gmail.com>
2020-03-23 20:22:39 -0500

    Imported Bootstrap

--- marketplace-client/src/App.js
+++ marketplace-client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/layouts/Header';
 import Navbar from './components/layouts/Navbar';
