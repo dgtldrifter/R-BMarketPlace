dab0c2aadd8e730479f35ff58cd7f06f94da3b83
burtonzr <burtonzr@gmail.com>
2020-03-26 19:25:15 -0500

    g

--- marketplace-client/src/components/pages/Category.js
+++ marketplace-client/src/components/pages/Category.js
@@ -21,32 +21,18 @@ class Category extends Component {
   }
 
   componentDidMount() {
-
     loadjs('../main.js');
-    if(localStorage.getItem('token') !== null) {
-      axios({
-        method: 'POST',
-        url: '../users/authToken',
-        headers: {
-            "Content-Type": "application/json",
-            'token': localStorage.getItem('token')
-        }
-      }).then((response) => {
-        
-      }).catch((error) => {
-          console.log(error);
-      });
-    } else {
-      localStorage.clear();
-    }
     axios({
       method: 'POST',
       url: '../posts/filterPosts',
       headers: {
         "Content-Type": "application/json"
+      },
+      data: {
+        categoryid: this.props.categoryExtra,
+        saletype: this.props.category
       }
     }).then((response) => {
-      console.log(response.data);
       this.setState({
         isLoaded: true,
         posts: response.data
@@ -57,58 +43,22 @@ class Category extends Component {
         error
       });
     });
-    
   }
 
   componentDidUpdate() {
-    
     //Update Category State on Page Change
     if(this.state.category !== this.props.category || this.state.categoryExtra !== this.props.categoryExtra){
-    this.setState({
-      category: this.props.category,
-      categoryExtra: this.props.categoryExtra
-    })
-  }
-
-
-    
-  
+      this.setState({
+        category: this.props.category,
+        categoryExtra: this.props.categoryExtra
+      });
+    }
   }
 
   render() {
     const { error, isLoaded } = this.state;
-    const postItemsFurniture = this.state.posts.slice(12, 15).map((post) => 
+    const posts = this.state.posts.map((post) => 
       <div key={post._id} className="col-lg-4 col-md-6 portfolio-item filter-transportation">
-        <div className="portfolio-wrap">
-          <img src={post.image} className="img-fluid" alt={post.name} />
-          <div className="portfolio-info">
-            <h4>{post.name}</h4>
-            <p>${post.price}</p>
-          </div>
-          <div className="portfolio-links">
-            <a href={post.image} data-gall="portfolioGallery" className="venobox" title={post.name}><i className="bx bx-plus"></i></a>
-            <a href="/" title="More Details"><i className="bx bx-link"></i></a> 
-          </div>
-        </div>
-      </div>
-    );
-    const postItemsTransportation = this.state.posts.slice(9, 12).map((post) => 
-      <div key={post._id} className="col-lg-4 col-md-6 portfolio-item filter-furniture">
-        <div className="portfolio-wrap">
-          <img src={post.image} className="img-fluid" alt={post.name} />
-          <div className="portfolio-info">
-            <h4>{post.name}</h4>
-            <p>${post.price}</p>
-          </div>
-          <div className="portfolio-links">
-            <a href={post.image} data-gall="portfolioGallery" className="venobox" title={post.name}><i className="bx bx-plus"></i></a>
-            <a href="/" title="More Details"><i className="bx bx-link"></i></a> 
-          </div>
-        </div>
-      </div>
-    );
-    const postItemsCooking = this.state.posts.slice(1, 4).map((post) => 
-      <div key={post._id} className="col-lg-4 col-md-6 portfolio-item filter-cooking-appliances">
         <div className="portfolio-wrap">
           <img src={post.image} className="img-fluid" alt={post.name} />
           <div className="portfolio-info">
@@ -138,18 +88,16 @@ class Category extends Component {
                 </div>
                 <div id="searchBox">
                 <input
-                        type='text'
-                        placeholder="Search Here..."
-                        className="form-control"
-                        onChange={this.myChangeHandler}
+                  type='text'
+                  placeholder="Search Here..."
+                  className="form-control"
+                  onChange={this.myChangeHandler}
                 />
                 <br></br><br></br>
                 <h2>Search Bar User Input ->  {this.state.search}</h2>
                 </div>
                 <div className="row portfolio-container" style={{paddingTop: "50px"}}>
-                  {postItemsCooking}
-                  {postItemsTransportation}
-                  {postItemsFurniture}
+                  {posts}
                 </div>
               </div>
             </section>
